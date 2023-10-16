8e26c3847a18ce87b1a96b3527ed6c502a80fe81
mnerahawn <mnerahawncs@gmail.com>
2023-10-16 10:22:06 +0300

    Initial Commit

---
+++ MyMoney2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMoney2
+//
+//  Created by mnerah awn on 01/04/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMoney2/MyMoney2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoney2App.swift
+//  MyMoney2
+//
+//  Created by mnerah awn on 01/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyMoney2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
