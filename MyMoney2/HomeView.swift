af088105476ac63ece5655976bfaa7368190f038
Yasmintao <yasminhassaniah00@gmail.com>
2023-10-16 12:11:59 +0300

    Home screen, Updeted

---
+++ MyMoney2/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MyMoney2
+//
+//  Created by Yasmin on 01/04/1445 AH.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
