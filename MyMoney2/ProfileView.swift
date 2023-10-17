6756f622112fd0b0b6424877450cbdbeaacca26e
RQ05 <reyoffmajed@gmail.com>
2023-10-17 12:15:39 +0300

    profile view

---
+++ MyMoney2/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  MyMoney2
+//
+//  Created by Reyouf Alqahtani on 02/04/1445 AH.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
